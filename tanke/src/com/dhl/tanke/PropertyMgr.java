e9b4e3a237b813361a6153a7bf4c2847379b2ca8
denghailing <940424671@qq.com>
2020-06-01 23:28:43 +0800

    改写，加入设计模式重构代码

--- tanke/src/com/dhl/tanke/PropertyMgr.java
+++ tanke/src/com/dhl/tanke/PropertyMgr.java
@@ -14,23 +14,25 @@ import org.junit.experimental.theories.Theories;
  * @version 2020年5月31日
  */
 //将该类设计成单例模式：在内存中如果需要某个类的对象，在程序上保证有且仅有一个该类的对象。
-public class PropertyMgr {
-	//该类里面存了好多键值对。提供方法访问这些键值对
-	static Properties properties = new Properties();
-	static{
-		try {
-			properties.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public static Object get(String key){
-		if(properties == null) return null;
-		return properties.get(key);
-	}
-	//getInt(key)
-	//getString(key)
-//	public static void main(String[] args){
-//		System.out.println(properties.get("initTankeCount"));
+// 单例：PropertyMgr, ResourceMgr
+//策略：Strategy   a: Comparable b:Compatator  c:tanke fire
+//public class PropertyMgr {
+//	//该类里面存了好多键值对。提供方法访问这些键值对
+//	static Properties properties = new Properties();
+//	static{
+//		try {
+//			properties.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 //	}
-}
+//	public static Object get(String key){
+//		if(properties == null) return null;
+//		return properties.get(key);
+//	}
+//	//getInt(key)
+//	//getString(key)
+////	public static void main(String[] args){
+////		System.out.println(properties.get("initTankeCount"));
+////	}
+//}
